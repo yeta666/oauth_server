3b03488f3540d61499bf5fea73ca42003917d2e6
陶杰 <429721783@qq.com>
2017-05-14 18:56:15 +0800

    授权服务器v1.0

---
+++ src/main/java/cn/com/sina/alan/oauth/config/AlanOAuthWebConfig.java
@@ -0,0 +1,39 @@
+package cn.com.sina.alan.oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+/**
+ * 配置用户信息，以及受保护路径、允许访问路径
+ */
+@Configuration
+public class AlanOAuthWebConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/favor.ico");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+}
